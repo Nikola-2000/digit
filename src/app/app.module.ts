ce616d7d69845f40b3a93b45b94b7e1a9c898f00
Stefan <stefan.krsteski@gmail.com>
2021-12-01 01:58:39 +0100

    Branche zhivna so stil

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, Pipe } from '@angular/core'
+import { SafePipeModule } from 'safe-pipe'
+import {BrowserModule} from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    SafePipeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
